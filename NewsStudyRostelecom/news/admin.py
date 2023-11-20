e2a8e3eecdfc63bb9cd4691d4f658621f0a87682
Muzhik75 <muzhik75@gmail.com>
2023-11-20 22:45:03 +0300

    Первое ДЗ.

---
+++ NewsStudyRostelecom/news/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . models import *
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'date']
+    list_filter = ['title', 'author', 'date']
+
+admin.site.register(Article, ArticleAdmin)
---
+++ NewsStudyRostelecom/news/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse
+from .models import  *
+
+
+def index(request):
+    article = Article.objects.all().first()
+    context = {'article':article}
+    return render(request,'news/index.html', context)
+
+def news(request):
+    return render(request,'news/news.html')
+
+def detail(request, id):
+    article = Article.objects.filter(id=id)
+    return HttpResponse(f'<h1>{article.title}</h1>')
